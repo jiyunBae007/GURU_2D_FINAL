e9567f86cc7ed41d1a35caeb8bce52eac72b11a0
BaejiYun <331lucy@naver.com>
2020-08-19 15:01:34 +0900

    기능적 구현 최종
    gameover과 restart에 관한 부분에 대한 수정으로 기능적 구현을 완료함

---
+++ Assets/GameManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] NumberImage;
+    public Sprite[] Number;
+    
+    public Image TimeBar;
+
+    public GameObject EndPanel;
+
+    private void Update()
+    {
+        int temp2 = DataManager.Instance.score % 1000;
+
+        temp2 = temp2 / 100;
+        NumberImage[0].GetComponent<Image>().sprite = Number[temp2];
+
+        int temp3 = DataManager.Instance.score % 100;
+
+        temp3 = temp3 / 10;
+        NumberImage[1].GetComponent<Image>().sprite = Number[temp3];
+
+        int temp4 = DataManager.Instance.score % 10;
+        NumberImage[2].GetComponent<Image>().sprite = Number[temp4];
+
+        if (!DataManager.Instance.PlayerDie)
+        {
+            DataManager.Instance.playTimeCurrent -= 1 * Time.deltaTime;
+
+            TimeBar.fillAmount = DataManager.Instance.playTimeCurrent / DataManager.Instance.playTimeMax;
+
+            if (DataManager.Instance.playTimeCurrent < 0)
+            {
+                DataManager.Instance.PlayerDie = true;
+            }
+
+            if (DataManager.Instance.margnetTimeCurrent > 0)
+            {
+                DataManager.Instance.margnetTimeCurrent -= 1 * Time.deltaTime;
+            }
+        }
+
+        if (DataManager.Instance.PlayerDie == true)
+        {
+            EndPanel.SetActive(true);
+        }
+    }
+
+    public void Restart_Btu()
+    {
+        DataManager.Instance.stage = 0;
+        DataManager.Instance.stageView = 0;
+        DataManager.Instance.score = 0;
+        DataManager.Instance.PlayerDie = false;
+        DataManager.Instance.playTimeCurrent = DataManager.Instance.playTimeMax;
+        DataManager.Instance.margnetTimeCurrent = 0;
+
+        SceneManager.LoadScene("SampleScene");
+    }
+
+}
